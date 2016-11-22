be2b9a45f2b8894c06193ced7ef8db748054936e
Oksert <kostyaoksert@gmail.com>
2016-11-23 00:47:20 +0300

    1

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 (function () {
-    var app = angular.module("LunchCheck",[]);
-    app.controller("LunchCheckController", LunchCheckController);
-    LunchCheckController.$inject = ['$scope'];
-    function LunchCheckController ($scope) {
-        $scope.createMessage = function () {
-          $scope.message = calcNumber($scope.dishes);
-        }
-        function calcNumber (str) {
-          if (!str) {
-            return "Please enter data first";
-          }
-          var allItems = str.split(',');
-          if (allItems.length > 3) {
-            return "Too much!";
-          } else {
-            return "Enjoy!";
-          }
-        }
+    var app = angular.module("ShoppingListCheckOff",[]);
+    app.controller("ToBuyController", ToBuyController);
+    app.controller("AlreadyBoughtController", AlreadyBoughtController);
+    app.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+    function ToBuyController (ShoppingListCheckOffService) {
+      var self = this;
+      self.items = ShoppingListCheckOffService.getToBuyItems();
+      self.buyItem = function(idx) {
+        ShoppingListCheckOffService.checkOutItem(idx);
+      }
+    }
+    function AlreadyBoughtController (ShoppingListCheckOffService) {
+      var self = this;
+      self.items = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService () {
+      var self = this;
+      var toBuyItems = [
+        { name: "cookies", quantity: 10 },
+        { name: "drinks", quantity: 5 },
+        { name: "chips", quantity: 3 },
+        { name: "apples", quantity: 20 },
+        { name: "oranges", quantity: 3 }
+      ];
+      var boughtItems = [];
+      self.checkOutItem = function (idx) {
+        boughtItems.push(toBuyItems[idx]);
+        toBuyItems.splice(idx,1);
+      }
+      self.getToBuyItems = function () {
+        return toBuyItems;
+      }
+      self.getBoughtItems = function () {
+        return boughtItems;
+      }
+
     }
 
 })();
